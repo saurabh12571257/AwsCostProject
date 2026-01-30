5a00884fde328be942af8cfa9b44bc3958cb421c
saurabh12571257 <davesaurabh59@gmail.com>
2026-01-30 13:22:06 +0530

    s3 config encryption added

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -25,6 +25,16 @@ resource "aws_s3_bucket_public_access_block" "cost_data_block_public" {
   restrict_public_buckets = true
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "cost_data_encryption" {
+  bucket = aws_s3_bucket.cost_data_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 resource "random_id" "bucket_id" {
   byte_length = 4
 }
