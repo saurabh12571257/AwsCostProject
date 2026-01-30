c53d1130e053fd7fc21e3989c39f1fb23d290a83
saurabh12571257 <davesaurabh59@gmail.com>
2026-01-30 13:39:03 +0530

    dynamodb.tf config done

---
+++ terraform/dynamodb.tf
@@ -0,0 +1,23 @@
+resource "aws_dynamodb_table" "cost_history" {
+  name         = "aws-cost-history"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key  = "service_name"
+  range_key = "date"
+
+  attribute {
+    name = "service_name"
+    type = "S"
+  }
+
+  attribute {
+    name = "date"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "aws-cost-history"
+    Project     = "aws-cost-optimizer"
+    Environment = "Dev"
+  }
+}
